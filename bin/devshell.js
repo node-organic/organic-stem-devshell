9414fc0e4536be7d8debd9e931616b202f58012a
outbounder <obiwonn@gmail.com>
2019-04-15 18:49:43 +0300

    fix devshell entry point

--- bin/devshell.js
+++ bin/devshell.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const path = require('path')
 const opn = require('opn')
+process.env.CELL_MODE = '_production'
 process.env.PRJROOT = process.cwd()
 process.chdir(path.join(__dirname, '../cells/server'))
 let server = require('../cells/server')
-server.start('_production')
+server.start()
 opn('http://localhost:8787')
